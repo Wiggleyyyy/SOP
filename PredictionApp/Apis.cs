2e1cc6c3858126b897ac67b270f2c3e64d36deda
David <davi5145@edu.sde.dk>
2024-05-22 08:39:01 +0200

    upload prediction app

---
+++ PredictionApp/Apis.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Printing;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PredictionApp
+{
+    public class NationalizeApiClient
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task<CountryPrediction> GetCountryPredictionAsync(string name)
+        {
+            string url = $"https://api.nationalize.io?name={name}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                NationalizeResponse apiRespone = JsonConvert.DeserializeObject<NationalizeResponse>(responseBody);
+
+                if (apiRespone != null && apiRespone.Country.Any())
+                {
+                    CountryPrediction countryWithHighestProbability = apiRespone.Country.OrderByDescending(x => x.probability).FirstOrDefault();
+                    return countryWithHighestProbability;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+
+    public class GenderizeApiClient
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task<GenderPrediction> GetGenderPredictionAsync(string name, string country_id)
+        {
+            string url = $"https://api.genderize.io?name={name}&country_id={country_id}";
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Error fetching data from API: {response.ReasonPhrase}");
+            }
+
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+            GenderPrediction genderPrediction = JsonConvert.DeserializeObject<GenderPrediction>(jsonResponse);
+
+            return genderPrediction;
+        }
+    }
+
+    public class AgifyApiClient
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task<AgePrediciton> GetAgePredicitonAsync(string name, string country_id)
+        {
+            string url = $"https://api.agify.io?name={name}&country_id={country_id}";
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Error fetching data from API: {response.ReasonPhrase}");
+            }
+
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+            AgePrediciton agePrediciton = JsonConvert.DeserializeObject<AgePrediciton>(jsonResponse);
+
+            return agePrediciton;
+        }
+    }
+}
---
+++ PredictionApp/DataModels.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PredictionApp
+{
+    public class CountryPrediction
+    {
+        public string country_id { get; set; }
+        [JsonProperty("probability")]
+        public float probability { get; set; }
+    }
+
+    public class NationalizeResponse
+    {
+        public string name { get; set; }
+        public List<CountryPrediction> Country = new List<CountryPrediction>();
+    }
+
+    public class GenderPrediction
+    {
+        public string name { get; set; }
+        public string gender { get; set; }
+        [JsonProperty("probability")]
+        public float probability { get; set; }
+        public int count { get; set; }
+    }
+
+    public class AgePrediciton
+    {
+        public string name { get; set; }
+        public int Age { get; set; }
+        [JsonProperty("probability")]
+        public float probability { get; set; }
+        public int count { get; set; }
+    }
+}
