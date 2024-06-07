c243773387e759b7f2d1fad4b8c4c02925f99df6
David <davi5145@edu.sde.dk>
2024-06-07 09:12:24 +0200

    upload fixed error with API

--- password_manager_electron/API/database.cs
+++ password_manager_electron/API/database.cs
@@ -158,6 +158,8 @@ namespace API
                                 encrypted_password = reader.GetString(reader.GetOrdinal("password")),
                             };
 
+                            login.password = DecryptPassword(login.encrypted_password, "AES");
+
                             if (login != null)
                             {
                                 loginsList.Add(login);
