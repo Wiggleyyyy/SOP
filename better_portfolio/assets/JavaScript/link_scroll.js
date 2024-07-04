367f6446ce63fc88d94ac6cb0e80d7c2f4798af2
David <davi5145@edu.sde.dk>
2024-07-04 14:27:29 +0200

    upload

--- better_portfolio/assets/JavaScript/link_scroll.js
+++ better_portfolio/assets/JavaScript/link_scroll.js
@@ -18,3 +18,38 @@ document.addEventListener('scroll', function() {
     let speed = 0.5;
     parallaxContent.style.transform = 'translateY(' + scrollPosition * -speed + 'px)';
 });
+
+document.addEventListener('scroll', function() {
+    const parallaxContent = document.querySelector('.card-container');
+    let scrollPosition = window.pageYOffset;
+    let speed = 0.5;
+    parallaxContent.style.transform = 'translateY(' + scrollPosition * -speed + 'px)';
+});
+
+document.addEventListener('scroll', function() {
+    const parallaxContent = document.querySelector('.birth-date');
+    let scrollPosition = window.pageYOffset;
+    let speed = 0.2;
+    parallaxContent.style.transform = 'translateY(' + scrollPosition * -speed + 'px)';
+});
+
+document.addEventListener('scroll', function() {
+    const parallaxContent = document.querySelector('.nationality');
+    let scrollPosition = window.pageYOffset;
+    let speed = 0.3;
+    parallaxContent.style.transform = 'translateY(' + scrollPosition * -speed + 'px)';
+});
+
+document.addEventListener('scroll', function() {
+    const parallaxContent = document.querySelector('.get-in-touch');
+    let scrollPosition = window.pageYOffset;
+    let speed = 0.2;
+    parallaxContent.style.transform = 'translateY(' + scrollPosition * -speed + 'px)';
+});
+
+document.addEventListener('scroll', function() {
+    const parallaxContent = document.querySelector('.experience');
+    let scrollPosition = window.pageYOffset;
+    let speed = 0.2;
+    parallaxContent.style.transform = 'translateY(' + scrollPosition * -speed + 'px)';
+});
